6015efc3293b1b464fb909498010819d2479c5ad
Scott Cyrus Jafary <scottcyrusj@gmail.com>
2021-08-06 20:32:40 +0700

    working on checkbox component

---
+++ src/components/Checkboxes.js
@@ -0,0 +1,16 @@
+import React {useState, useEffect } from 'react'
+
+export default function Checkboxes() {
+
+  const boxes = [
+    { label: "code", id: "code"},
+    { label: "ui", id: "ui"},
+    { label: "art", id: "art"}
+  ]
+
+  return (
+    <Checkbox>
+      
+    </div>
+  )
+}

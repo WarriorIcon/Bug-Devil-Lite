d1f7e9555e4d51b3148c9ccab9ecb4a689e10ace
Scott Cyrus Jafary <scottcyrusj@gmail.com>
2021-08-07 17:19:16 +0700

    WIP checkboxes component

--- src/components/Checkboxes.js
+++ src/components/Checkboxes.js
@@ -1,35 +1,48 @@
 import React, {useState, useEffect } from 'react'
 
 export default function Checkboxes() {
-  const [boxState, setBoxState] = useState()
+  const [boxState, setBoxState] = useState({ code: true })
 
-  const Checkboxes = () => {
-    const elements = boxes.map( boxData => {
-      <div>
-        <label
-          htmlFor={boxData.id}
-          key={boxData.label}
-          >{boxData.label}
-        </label>
-        <input
-          id={}
-          type="checkbox" 
-        />
-      </div>
-    })
+  useEffect( () => console.log(boxState), [boxState])
+
+  function handleCheckboxes(boxId) {
+    setBoxState((prevState) => ({
+      ...prevState,
+      [boxId]: !prevState[boxId]
+    }))
   }
+
+  return ( 
+    <div>
+      {boxes.map((boxData) => {
+        <div>
+          <label
+            htmlFor={boxData.id}
+            key={boxData.label}
+            >{boxData.label}
+          </label>
+          <input
+            id={boxData.id}
+            type="checkbox"
+            checked={boxState.boxId}
+            onChange={() => handleCheckboxes(boxData.id)}  
+          />
+        </div> }
+      )}
+    </div>
+  )
+}
   const boxes = [
     { label: "code", id: "code"},
     { label: "ui", id: "ui"},
     { label: "art", id: "art"}
   ]
 
-  return (
-    <Checkboxes>
-      
-    </div>
-  )
-}
+
+
+
+
+
 
 
 
